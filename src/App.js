fcd3aad7c8fd5e035f32fe3da5fab2dec1de8747
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-11-26 18:02:05 +0100

    First init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useRef} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as bodyPix from '@tensorflow-models/body-pix';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
+import {drawBody} from './utilities';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+ const runhandPose = async () => {
+   const net = await handpose.load()
+   console.log('handPose is loaded');
+
+   setInterval( () => {
+     detectBody(net)
+   },100)
+ }
+
+ const detectBody = async (net) => {
+   if(typeof webcamRef.current !== "undefined" 
+        && webcamRef.current !== null
+        && webcamRef.current.video.readyState === 4 ) {
+        
+          const video = webcamRef.current.video;
+          const videoWidth = webcamRef.current.video.videoWidth;
+          const videoHeight = webcamRef.current.video.videoHeight;
+
+          webcamRef.current.video.width = videoWidth;
+          webcamRef.current.video.height = videoHeight;
+
+          canvasRef.current.width = videoWidth;
+          canvasRef.current.height = videoHeight;
+
+          const body = await net.estimateHands(video);
+          console.log(body);
+
+          const ctx = canvasRef.current.getContext("2d");
+          drawBody(body, ctx);
+
+   }
+ }
+
+
+ runhandPose();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
+      />
+      <canvas ref={canvasRef}
+         style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zindex: 9,
+          width: 640,
+          height: 480,
+         }}
+      />
     </div>
   );
 }
